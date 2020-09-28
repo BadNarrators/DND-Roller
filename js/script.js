25e0126301b2227b33d3fb6e6345555bcfc4dd9d
BadNarrators <andreaatzei01@gmail.com>
2020-09-28 20:21:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 function loadPg(){
     var array = document.getElementById('pgData').value.split(/\r?\n/);
 
@@ -12,17 +36,48 @@ function loadPg(){
     pg.nowHealth = pgHealthArr[0].replace( /^\D+/g, '');
     pg.maxHealth = pgHealthArr[1];
     pg.str = pgStats1[0].substring(5,7);
-    pg.dex = pgStats1[1].substring(5,7);
-    pg.con = pgStats1[2].substring(5,7);
+    pg.strMod = Math.floor((parseInt(pg.str)-10)/2);
+    pg.dex = pgStats1[1].substring(6,8);
+    pg.dexMod = Math.floor((parseInt(pg.dex)-10)/2);
+    pg.con = pgStats1[2].substring(6,8);
+    pg.conMod = Math.floor((parseInt(pg.con)-10)/2);
     pg.int = pgStats2[0].substring(5,7);
-    pg.wis = pgStats2[1].substring(5,7);
-    pg.cha = pgStats2[2].substring(5,7);
+    pg.intMod = Math.floor((parseInt(pg.int)-10)/2);
+    pg.wis = pgStats2[1].substring(6,8);
+    pg.wisMod = Math.floor((parseInt(pg.wis)-10)/2);
+    pg.cha = pgStats2[2].substring(6,8);
+    pg.chaMod = Math.floor((parseInt(pg.cha)-10)/2);
+    pg.saveProf = [];
+    array[8].slice(-(array[8].length-19)).split(",").forEach(function(k){
+        k = k.substring(1, k.length-3).replace(/\s/g, '');
+        switch(k){
+            case("Strength"): pg.saveProf.push("str"); break;
+            case("Dexterity"): pg.saveProf.push("dex"); break;
+            case("Constitution"): pg.saveProf.push("con"); break;
+            case("Intelligence"): pg.saveProf.push("int"); break;
+            case("Wisdom"): pg.saveProf.push("wis"); break;
+            case("Charisma"): pg.saveProf.push("cha"); break;
+        }
+    });
+    pg.skillProf = [];
+    array[9].slice(-(array[9].length-20)).split(",").forEach(function(k){
+        if(k.substring(k.length-4, k.length-1) == "adv") {
+            k = k.substring(0, k.length-5);
+        }
+        k = k.substring(1, k.length-3).replace(/\s/g, '');
+        pg.skillProf.push(k);
+    });
 
     var jsonString= JSON.stringify(pg);
 
+    //eraseCookie(pg.name);
+    setCookie(pg.name, pg, 1);
+    if(getCookie(pg.name)) console.log("true");
+    else (console.log("false"));
 
     console.log(JSON.parse(jsonString));
 }
+
 
 /*Kravin Sanguemarcio
 Goblin Blood Hunter 5
