296f30985fbaf3f8fceaa38f2e54a7d07840995c
BadNarrators <andreaatzei01@gmail.com>
2020-09-28 06:30:14 +0200

    changed a bunch

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function loadPg(){
+    var array = document.getElementById('pgData').value.split(/\r?\n/);
+
+    var pg = new Object();
+    pgHealthArr = array[4].split("/");
+    pg.name = array[0];
+    pg.class = array[1];
+    pg.proficiency = array[2].slice(-2);
+    pg.ac = array[3].slice(-2);
+    pg.nowHealth = pgHealthArr[0].replace( /^\D+/g, '');
+    pg.maxHealth = pgHealthArr[1];
+
+    var jsonString= JSON.stringify(pg);
+
+
+    console.log(JSON.parse(jsonString));
+}
+
+/*Kravin Sanguemarcio
+Goblin Blood Hunter 5
+Proficiency Bonus: +3
+AC: 18
+HP: 20/46
+Initiative: +5
+STR: 13 (+1) DEX: 20 (+5) CON: 16 (+3)
+INT: 19 (+4) WIS: 12 (+1) CHA: 10 (+0)
+Save Proficiencies: Dexterity +8, Intelligence +7
+Skill Proficiencies: Acrobatics +8, Insight +4, Investigation +7, Religion +7, Stealth +5 (adv), Survival +4
+Senses: passive Perception 11
+Attacks
+Crossbow, Light: Attack: +8 to hit. Hit: 1d8+5 [piercing] damage.
+Dagger: Attack: +8 to hit. Hit: 1d4+5 [piercing] damage.
+Dart: Attack: +8 to hit. Hit: 1d4+5 [piercing] damage.
+Rapier: Attack: +8 to hit. Hit: 1d8+5 [piercing] damage.
+Rapier, +1: Attack: +9 to hit. Hit: 1d8+6 [magical piercing] damage.
+Rifle, Hunting: Attack: +5 to hit. Hit: 2d10+5 [piercing] damage.
+Unarmed Strike: Attack: +4 to hit. Hit: 2 [bludgeoning] damage.*/
